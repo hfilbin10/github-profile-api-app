db87b5cbc527eb1c6d9edfd26ff012cfba11667e
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-11-02 17:45:24 -0500

    Github API app

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import axios from "axios"
+import MyNavBar from './Components/MyNavBar';
+import MyModal from './Components/MyModal';
+import "./App.css"
+
+
+
+
+function App() {
+  const [userData, setUserData] = useState({})
+  const [showModal, setShowModal] = useState(false)
+  const [searchLogin, setSearchLogin] = useState("")
+
+  const getUserData = async (e) => {
+    setShowModal(true)
+    e.preventDefault();
+    let login = e.target.value
+    if (login !== "None") {
+      try {
+        const response = await axios.get(`https://api.github.com/users/${login}`)
+        const newObj = { login: response.data.login, avatar: response.data.avatar_url, url: response.data.html_url, followers: response.data.followers, following: response.data.following, reposAmount: response.data.public_repos, name: response.data.name, bio: response.data.bio }
+        setUserData(newObj)
+      } catch (err) {
+        console.log("Something went wrong")
+        console.log(err)
+      }
+    } else {
+      setUserData({})
+    }
+
+  }
+
+  const hideModal = () => {
+    setShowModal(false)
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setShowModal(true)
+    try {
+      const response = await axios.get(`https://api.github.com/users/${searchLogin}`)
+      const newObj = { login: response.data.login, avatar: response.data.avatar_url, url: response.data.html_url, followers: response.data.followers, following: response.data.following, reposAmount: response.data.public_repos, name: response.data.name, bio: response.data.bio }
+      setUserData(newObj)
+    } catch (err) {
+      console.log("Something went wrong")
+      console.log(err)
+    }
+
+  }
+
+  const handleChange = (e) => {
+    setSearchLogin(e.target.value)
+  }
+
+
+  return (
+    <>
+      <MyNavBar getUserData={getUserData} handleSubmit={handleSubmit} handleChange={handleChange} />
+      {showModal && <MyModal userData={userData} hideModal={hideModal} />}
+    </>
+  )
+}
+
+export default App
+
+
